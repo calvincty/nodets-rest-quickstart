0ec22dfc47e94774afc46bea99131cf47e37f067
Calvin Chong <calvincty@gmail.com>
2021-05-14 13:34:44 +0800

    remove build dir

--- dist/routes/books.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-var express_1 = __importDefault(require("express"));
-var book_1 = __importDefault(require("../controllers/book"));
-var router = express_1.default.Router();
-router.get('/', book_1.default.getBooks);
-module.exports = router;
